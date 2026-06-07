c6328f8c695175c1665b0a0e745c95cdaf0384e3
Nayeli Córdova Mendoza <nayeligiselle.0206@gmail.com>
2026-06-07 18:15:14 +0200

    Exercice 1 done

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,22 @@
 //Inicializa la clase Welcome
 //Crea el método main
 
+public class Welcome {
+
+    public static void main(String[] args){
+        greeting();
+        greetingCoder("Nayeli");
+    }
+
+    public static void greeting(){
+        System.out.println("¡Hola FemCoder!");
+    }
+
+    public static void greetingCoder(String coderName){
+        System.out.println("¡Hola "+coderName+"!");
+    }
+}
+
 //Ejercicio 1: Define el método greeting que imprima en terminal ¡Hola FemCoder!
 //Ejecuta el método greeting 
 
