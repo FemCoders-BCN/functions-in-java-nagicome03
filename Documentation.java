26e6575927d723744b4b4129204285731252de17
nagicome03 <nayeligiselle.0206@gmail.com>
2026-06-08 00:10:42 +0200

    Exercice 2 done

--- Documentation.java
+++ Documentation.java
@@ -7,6 +7,7 @@ public class Documentation {
     public static void main(String[] args) {
         greeting();
         System.out.println(greetingCoder("Alex", 41));
+        System.out.println(callingAGoddess("Artemisa"));
     }
 
     /**
@@ -36,6 +37,35 @@ public class Documentation {
      }
 
      //Ejercicio 1: Escribe un método llamado "callingAGoddess" que devuelva un String con una descripción de cada diosa de la mitología griega y romana al pasarle por parámetro un nombre. Documenta la función.
+
+    /**
+     * Function name: callingAGoddess
+     *
+     * @param name (String)
+     * @return description (String)
+     *
+     * Inside the function:
+     * 1. return the goddess description that corresponds to the goddess name.
+     */
+
+    public static String callingAGoddess(String name){
+        switch(name){
+            case "Afrodita":
+                return ("Hola soy Afrodita, la diosa del amor y la belleza.");
+            case "Atenea":
+                return ("Hola soy Atenea, la diosa de la sabiduría y la guerra estratégica.");
+            case "Artemisa":
+                return ("Hola soy Artemisa, la diosa de la caza y la naturaleza salvaje.");
+            case "Hera":
+                return ("Hola soy Hera, la diosa de los dioses y de la familia.");
+            case "Deméter":
+                return ("Hola soy Deméter, la diosa de la cosecha y la agricultura.");
+            case "Perséfone":
+                return ("Hola soy Perséfone, la diosa de la primavera y el inframundo.");
+            default:
+                return ("¡no la hemos encontrado aún!");
+        }
+    }
 
     /* 
     Afrodita: Hola soy Afrodita, la diosa del amor y la belleza.
